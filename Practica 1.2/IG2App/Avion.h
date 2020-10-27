0626fec00e9838139059dcfc917aecc89eb3c91f
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2020-10-27 14:50:40 +0100

    practica 1.2

--- Practica 1.2/IG2App/Avion.h
+++ Practica 1.2/IG2App/Avion.h
@@ -4,41 +4,8 @@
 class Avion:public OgreBites::InputListener
 {
 public:
-	Avion(Ogre::SceneNode* node){
-		mNode = node;
-	Ogre::SceneManager* mSM = mNode->getCreator();
-	
-	
-	Ogre::Entity* ent = mSM->createEntity("sphere.mesh");
-	cuerpoNode = mNode->createChildSceneNode("cuerpoNode");
-	cuerpoNode->attachObject(ent);
-	ent = mSM->createEntity("cube.mesh");
-	AlaINode = mNode->createChildSceneNode("AlaInode");
-	AlaINode->attachObject(ent);
-	ent = mSM->createEntity("cube.mesh");
-	AlaDNode = mNode->createChildSceneNode("AlaDnode");
-	AlaDNode->attachObject(ent);
-	ent = mSM->createEntity("Barrel.mesh");
-	frenteNode = mNode->createChildSceneNode("frenteNode");
-	frenteNode->attachObject(ent);
-	ent = mSM->createEntity("ninja.mesh");
-	pilotoNode = mNode->createChildSceneNode("PilotoInode");
-	pilotoNode->attachObject(ent);
-	aspas1 = new AspasMolino(mNode->createChildSceneNode("helice1node"), 6,"asdfg");
-	helice1Node = aspas1->getNode();
-	aspas2 = new AspasMolino(mNode->createChildSceneNode("helice2node"), 6,"aea");
-	helice2Node = aspas2->getNode();
-	helice1Node->scale(.1, .1, .1);
-	helice2Node->scale(.1,.1,.1);
-	AlaDNode->translate(200, 0, 0);
-	AlaDNode->scale(2.3, .2, 1);
-	AlaINode->translate(-200, 0,0);
-	AlaINode->scale(2.3, .2, 1);
-	pilotoNode->yaw(Degree(180));
-	pilotoNode->translate(0,-50,0);
-
-	}
-	~Avion() { mNode->removeAndDestroyAllChildren(); }
+	Avion(Ogre::SceneNode* node); 
+	~Avion() { mNode->removeAndDestroyAllChildren(); delete aspas1; delete aspas2; }
 	// M�todos de InputListener que pueden redefinirse
 
 	virtual void frameRendered(const Ogre::FrameEvent& evt) { }
