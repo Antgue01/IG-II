c9d338058d0d942ef81fe7dbdbfd996562f8646b
Alonefcp <fcuadra@ucm.es>
2020-12-08 13:14:37 +0100

    shader luz

--- Practica 2.1/media/IG2App/AgujerosFS.glsl
+++ Practica 2.1/media/IG2App/AgujerosFS.glsl
@@ -1,17 +1,62 @@
 #version 330 core
+uniform mat4 modelViewMat;
+uniform mat4 normalMat;
+
+uniform vec3 lightAmbient; 
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
+
+uniform vec3 materialDiffuse;
+
 uniform sampler2D texturaL;
 uniform sampler2D texturaM;
 uniform vec4 InColor;
 uniform vec4 OutColor;
+
 in vec2 vUv0; 
+in vec3 vXxxNormal; 
+in vec4 vXxxVertex; 
 out vec4 fFragColor;
-void main() {
+float diff(vec3 cVertex, vec3 cNormal)
+{
+  vec3 lightDir = lightPosition.xyz;
+
+  if(lightPosition.w == 1) lightDir = lightPosition.xyz - cVertex;
+
+  return max(dot(cNormal,normalize(lightDir)),0.0);
+}
+
+
+void main() 
+{
+
+vec4 frontColor;
+vec4 backColor;
+
+
+// ambient
+vec3 ambient = lightAmbient * materialDiffuse;
+// diffuse en view space
+vec3 viewVertex = vec3(modelViewMat * vXxxVertex);
+vec3 viewNormal = normalize(vec3(normalMat * vec4(vXxxNormal,0)));
+vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
+
+//frontColor = ambient + diffuse; // + specular
+frontColor = vec4(ambient + diffuse,1.0);
+
+diffuse = diff(viewVertex,-viewNormal) * lightDiffuse * materialDiffuse;
+
+//backColor = ambient + diffuse; // + specular
+backColor = vec4(ambient + diffuse,1.0);
+
+
 vec3 textColor = vec3(texture(texturaL, vUv0)); 
 if(textColor.r < 0.5)
     discard;
 if(gl_FrontFacing)    
-    fFragColor = vec4(texture(texturaM,vUv0)) * OutColor;
+    fFragColor = vec4(texture(texturaM,vUv0)) * frontColor * OutColor;
 else
-    fFragColor = InColor;
-// fFragColor=vec4(textColor,1);
+    fFragColor = backColor * InColor;
+
+
 }
--- Practica 2.1/media/IG2App/AgujerosVS.glsl
+++ Practica 2.1/media/IG2App/AgujerosVS.glsl
@@ -1,9 +1,19 @@
 #version 330 core
 in vec4 vertex; 
+in vec3 normal;
 in vec2 uv0; 
 uniform mat4 modelViewProjMat; 
 out vec2 vUv0;
+out vec3 vXxxNormal; 
+out vec4 vXxxVertex; 
+
 void main() {
+
 vUv0 = uv0; 
+
+vXxxNormal = normal;
+vXxxVertex = vertex;
+
 gl_Position = modelViewProjMat * vertex; 
+
 }
